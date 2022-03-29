e2a2a1bb4610fae3b3f0e4cae1319236dbbdb569
Thibault Pellerin <tpellerin@wynd.eu>
2022-03-29 16:53:35 +0200

    1.2 stuff

--- AA_training.lua
+++ AA_training.lua
@@ -15,48 +15,41 @@ local function ActivateGroup( groupName )
 end
 
 local function ActivateMagic()
-    MESSAGE:New( "MAGIC est actif sur 265.000Mhz", 5 ):ToAll()
+    MESSAGE:New( "MAGIC est actif sur 125.000Mhz", 5 ):ToAll()
     local group = GROUP:FindByName( "Magic" )
     group:Activate()
 end
 
 local function ActivateOverlord()
-    MESSAGE:New( "OVERLORD est actif sur 266.000Mhz", 5 ):ToAll()
+    MESSAGE:New( "OVERLORD est actif sur 126.000Mhz", 5 ):ToAll()
     local group = GROUP:FindByName( "Overlord" )
     group:Activate()
 end
 
 local function ActivateTexaco()
-    MESSAGE:New( "TEXACO est actif : 265.000Mhz / TCN 1X A22 300kts", 10 ):ToAll()
+    MESSAGE:New( "TEXACO est actif : 125.000Mhz / TCN 1X A22 300kts", 10 ):ToAll()
     local group = GROUP:FindByName( "Texaco" )
     group:Activate()
 end
 
 local function ActivateArco()
-    MESSAGE:New( "ARCO est actif : 266.000Mhz / TCN 2X A22 300kts", 10 ):ToAll()
+    MESSAGE:New( "ARCO est actif : 126.000Mhz / TCN 2X A22 300kts", 10 ):ToAll()
     local group = GROUP:FindByName( "Arco" )
     group:Activate()
 end
 
 local function ActivateRedCap( groupName )
-
-    local PatrolZones = {}
-
     US_PlanesClientSet = SET_CLIENT:New():FilterCoalitions( "blue" ):FilterCategories( "plane" ):FilterPrefixes( "TBOLT" ):FilterActive():FilterStart()
-
-    PatrolZone = ZONE:New( "Red Zone" )
-    EngageZoneGroup = GROUP:FindByName( "Yellow Zone" )
-    CapEngageZone = ZONE_POLYGON:New( "Yellow Zone", EngageZoneGroup )
 
     RU_PlanesSpawn = SPAWN
             :NewWithAlias( groupName, "REDAI" )
-            :InitRandomizeZones( { PatrolZone } )
+            :InitRandomizeZones( { RedZone } )
             :OnSpawnGroup( function ( spawnGroup )
 
-        PatrolZones[spawnGroup] = AI_CAP_ZONE:New( PatrolZone, 2000, 5000, 400, 600 )
+        PatrolZones[spawnGroup] = AI_CAP_ZONE:New( RedZone , 2000, 5000, 400, 600 )
         PatrolZones[spawnGroup]:SetControllable( spawnGroup )
-        PatrolZones[spawnGroup]:SetDetectionZone( CapEngageZone )
-        PatrolZones[spawnGroup]:SetEngageZone( CapEngageZone )
+        PatrolZones[spawnGroup]:SetDetectionZone( YellowZone )
+        PatrolZones[spawnGroup]:SetEngageZone( YellowZone )
         PatrolZones[spawnGroup]:__Start( 5 )
     end
     )
@@ -70,12 +63,6 @@ end
 
 
 local function ActivateBlueAI( mode )
-
-    PatrolZone = ZONE:New( "Blue Zone" )
-    EngageZoneGroup = GROUP:FindByName( "Yellow Zone" )
-    CapEngageZone = ZONE_POLYGON:New( "Yellow Zone", EngageZoneGroup )
-    local PatrolZones = {}
-
     US_PlanesSpawn = SPAWN
             :NewWithAlias( "TBOLT BLUE AI " .. mode, "BLUEAI" )
             :InitCleanUp( 20 )
@@ -83,17 +70,17 @@ local function ActivateBlueAI( mode )
     if (mode == "HOT") then
         FilterName = "TBOLT HOT"
     end
-    US_PlanesClientSet = SET_CLIENT:New():FilterCoalitions( "blue" ):FilterCategories( "plane" ):FilterPrefixes( FilterName ):FilterStart()
+    US_PlanesClientSet = SET_CLIENT:New():FilterCoalitions( "blue" ):FilterCategories( "plane" ):FilterPrefixes( "TBOLT HOT" ):FilterStart()
     US_AI_Balancer = AI_BALANCER:New( US_PlanesClientSet, US_PlanesSpawn )
     US_AI_Balancer:InitSpawnInterval(2)
     US_AI_Balancer:ReturnToHomeAirbase( 10000 )
 
     function US_AI_Balancer:OnAfterSpawned( SetGroup, From, Event, To, AIGroup )
 
-                PatrolZones[AIGroup] = AI_CAP_ZONE:New( PatrolZone, 5000, 10000, 400, 500 )
+                PatrolZones[AIGroup] = AI_CAP_ZONE:New( BlueZone , 5000, 10000, 400, 500 )
                 PatrolZones[AIGroup]:SetControllable( AIGroup )
-                PatrolZones[AIGroup]:SetDetectionZone( CapEngageZone )
-                PatrolZones[AIGroup]:SetEngageZone( CapEngageZone )
+                PatrolZones[AIGroup]:SetDetectionZone( YellowZone )
+                PatrolZones[AIGroup]:SetEngageZone( YellowZone )
                 PatrolZones[AIGroup]:__Start( 1 )
 
     end
@@ -102,11 +89,6 @@ local function ActivateBlueAI( mode )
 end
 
 local function ActivateRedAI()
-
-    RU_PlanesClientSet = SET_CLIENT:New():FilterCountries( "Russia" ):FilterCategories( "plane" )
-    RU_PlanesSpawn = SPAWN:New( "TBOLT RED AI" ):InitCleanUp( 20 )
-    RU_AI_Balancer = AI_BALANCER:New( RU_PlanesClientSet, RU_PlanesSpawn )
-    RU_AI_Balancer:InitSpawnInterval(5)
 
 
 end
@@ -121,90 +103,107 @@ local function KillAllRed()
 end
 
 
-local function InvincibleMode( switch )
+local function ImmortalMode( switch )
+
+
+    local tagName = ""
+    local TBOLTGroupsB = {
+        GROUP:FindByName( "TBOLT" .. tagName .. " 1-1"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " 1-2"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " 1-3"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " 1-4"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT 1-1"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT 1-2"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT 1-3"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT 1-4"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " M2000 1-1"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " M2000 1-2"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " M2000 1-3"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " M2000 1-4"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT M2000 1-1"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT M2000 1-2"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT M2000 1-3"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT M2000 1-4")
+    }
+    tagName = " RED"
+    local TBOLTGroupsR = {
+        GROUP:FindByName( "TBOLT" .. tagName .. " 1-1"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " 1-2"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " 1-3"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " 1-4"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT 1-1"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT 1-2"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT 1-3"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT 1-4"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " M2000 1-1"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " M2000 1-2"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " M2000 1-3"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " M2000 1-4"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT M2000 1-1"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT M2000 1-2"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT M2000 1-3"),
+        GROUP:FindByName( "TBOLT" .. tagName .. " HOT M2000 1-4")
+    }
+
+
     if(switch) then
-        MESSAGE:New( "Mode Immortel activé", 10 ):ToAll()
+        MESSAGE:New( "Mode Immortel activé !", 10 ):ToAll()
+        Trainer = MISSILETRAINER
+                :New( 200 )
+                :InitMessagesOnOff(true)
+                :InitAlertsToAll(false)
+                :InitAlertsHitsOnOff(true)
+                :InitAlertsLaunchesOnOff(false)
+                :InitBearingOnOff(false)
+                :InitRangeOnOff(false)
+                :InitTrackingOnOff(false)
+                :InitTrackingToAll(false)
+                :InitMenusOnOff(false)
+
     else
-        MESSAGE:New( "Mode Immortel désactivé", 10 ):ToAll()
+        Trainer.TrackingScheduler:Stop()
+        MESSAGE:New( "Mode Immortel désactivé !", 10 ):ToAll()
     end
 
-    BlueZoneGroup = GROUP:FindByName( "Blue Zone" )
-    BlueZone = ZONE_POLYGON:New( "Blue Zone", BlueZoneGroup )
-
-    local TBOLTGroups = {
-        GROUP:FindByName( "TBOLT 1-1"),
-        GROUP:FindByName( "TBOLT HOT 1-1"),
-        GROUP:FindByName( "TBOLT 1-2"),
-        GROUP:FindByName( "TBOLT HOT 1-2"),
-        GROUP:FindByName( "TBOLT 1-3"),
-        GROUP:FindByName( "TBOLT HOT 1-3"),
-        GROUP:FindByName( "TBOLT 1-4"),
-        GROUP:FindByName( "TBOLT HOT 1-4")
-    }
-
-    local TBOLTUnits = {
-        UNIT:FindByName("TBOLT 1-1"),
-        UNIT:FindByName("TBOLT 1-2"),
-        UNIT:FindByName("TBOLT 1-3"),
-        UNIT:FindByName("TBOLT 1-4"),
-        UNIT:FindByName("TBOLT HOT 1-1"),
-        UNIT:FindByName("TBOLT HOT 1-2"),
-        UNIT:FindByName("TBOLT HOT 1-3"),
-        UNIT:FindByName("TBOLT HOT 1-4")
-    }
-    for i,group in pairs(TBOLTGroups) do
-        if( group ~= nil) then
-            group:SetCommandImmortal( switch )
-        end
-    end
-
-    if( switch ) then
-        for i,unit in pairs(TBOLTUnits) do
-            if unit then
-                unit:HandleEvent( EVENTS.Hit )
-                function unit:OnEventHit( EventData )
-                    if(nil == string.find(EventData.WeaponName, "weapons.shells")) then
-
-                        MESSAGE:New( "Alerte : " .. EventData.IniTypeName .. " " .. EventData.IniUnitName .. " a tué " .. unit:GetPlayerName() .. " avec un " .. EventData.WeaponName .. " !" , 15 ):ToAll()
-
-                        unitGroup = unit:GetGroup()
-                        unitGroup:SetCommandInvisible(true)
-
-                        MESSAGE:New( "Vous êtes mort ! Veuillez retourner en Zone Bleue", 15 ):ToGroup( unitGroup )
-                    else
-                        -- Hit by mike mike
-                    end
-                end
-            end
-        end
-
-        local TestInZone = {}
-        for i,group in pairs(TBOLTGroups) do
-            TestInZone[group] = SCHEDULER:New( nil,
-                    function()
-                        if group:IsCompletelyInZone( BlueZone ) then
-                            group:SetCommandInvisible(false)
-                            --MESSAGE:New( "Zone Bleue atteinte, vous pouvez reprendre l'engagement",10 ):ToGroup( group )
-                        end
-                    end,{}, 0, 1 )
-        end
-    end
 
 
 end
 
-local function BlueAIFormation()
-    local BlueAISet = SET_GROUP:New():FilterCoalitions("blue"):FilterCategories("plane"):FilterPrefixes("BLUEAI"):FilterStart()
-    BlueAISet:Flush()
-    local LeaderUnit = UNIT:FindByName( "TBOLT 1-1" )
-    if(nil == LeaderUnit) then
-        LeaderUnit = UNIT:FindByName( "TBOLT HOT 1-1" )
+local function TrackingMode( switch )
+    Trainer.DetailsRangeOnOff = switch
+    Trainer.DetailsBearingOnOff = switch
+    Trainer.TrackingOnOff = switch
+    if( switch ) then
+        MESSAGE:New( "Tracking des missiles activé !", 10 ):ToAll()
+    else
+        MESSAGE:New( "Tracking des missiles désactivé !", 10 ):ToAll()
     end
-    local LargeFormation = AI_FORMATION:New( LeaderUnit, BlueAISet, "Center Wing Formation", "Briefing" )
+end
+
+local function AIFormation( team )
+    local tagName = ""
+    local teamName = "blue"
+    local prefix = "BLUEAI"
+    if (team == "R") then
+        tagName = " RED"
+        teamName = "red"
+        prefix = "REDAI"
+    end
+    local AISet = SET_GROUP:New():FilterCoalitions( teamName ):FilterCategories("plane"):FilterPrefixes( prefix ):FilterStart()
+    AISet:Flush()
+    local LeaderUnit = UNIT:FindByName( "TBOLT" .. tagName .. " 1-1" )
+    if(nil == LeaderUnit) then
+        LeaderUnit = UNIT:FindByName( "TBOLT" .. tagName .. " HOT 1-1" )
+    end
+    local LargeFormation = AI_FORMATION:New( LeaderUnit, AISet, "Center Wing Formation", "Briefing" )
     LargeFormation:FormationCenterWing( 500, 50, 0, 0, 250, 250 )
     LargeFormation:__Start( 1 )
 end
 
+local function MissileTrainer()
+
+
+end
 --
 -- FOX
 --
@@ -235,7 +234,15 @@ end
 _SETTINGS:SetPlayerMenuOff()
 local MenuRed = MENU_MISSION:New( "ROUGE" )
 local MenuBlue = MENU_MISSION:New( "BLEU" )
+local MenuImmortal = MENU_MISSION:New( "MODE IMMORTEL" )
 local MenuSupport = MENU_MISSION:New( "SUPPORT" )
+--local MenuTest = MENU_MISSION:New( "TESTS" )
+--local MenuScoring = MENU_MISSION:New( "SCORING" )
+
+MENU_MISSION_COMMAND:New( "MODE IMMORTEL ON", MenuImmortal, ImmortalMode, true )
+MENU_MISSION_COMMAND:New( "MODE IMMORTEL OFF", MenuImmortal, ImmortalMode, false )
+MENU_MISSION_COMMAND:New( "TRACKING ON", MenuImmortal, TrackingMode, true )
+MENU_MISSION_COMMAND:New( "TRACKING OFF", MenuImmortal, TrackingMode, false )
 
 MENU_MISSION_COMMAND:New( "Mig-21", MenuRed, ActivateRedCap, "Mig-21" )
 MENU_MISSION_COMMAND:New( "Su-27", MenuRed, ActivateRedCap, "Su-27" )
@@ -246,20 +253,27 @@ MENU_MISSION_COMMAND:New( "F-16 DOGFIGHT", MenuRed, ActivateRedCap, "F-16 DF" )
 MENU_MISSION_COMMAND:New( "F/A-18C DOGFIGHT", MenuRed, ActivateRedCap, "F-18 DF" )
 MENU_MISSION_COMMAND:New( "RESET TOUS LES ROUGES", MenuRed, KillAllRed )
 
-MENU_MISSION_COMMAND:New( "MODE IMMORTEL ON", MenuBlue, InvincibleMode, true )
-MENU_MISSION_COMMAND:New( "MODE IMMORTEL OFF", MenuBlue, InvincibleMode, false )
+
 MENU_MISSION_COMMAND:New( "TBOLT AI : DEPART HOT", MenuBlue, ActivateBlueAI, "HOT" )
 MENU_MISSION_COMMAND:New( "TBOLT AI : DEPART COLD", MenuBlue, ActivateBlueAI, "COLD" )
-MENU_MISSION_COMMAND:New( "TBOLT AI : SUIVRE TBOLT 1-1", MenuBlue, BlueAIFormation )
+MENU_MISSION_COMMAND:New( "TBOLT AI : SUIVRE TBOLT 1-1", MenuBlue, AIFormation )
 
 MENU_MISSION_COMMAND:New( "AWACS BLEU", MenuSupport, ActivateMagic )
 MENU_MISSION_COMMAND:New( "AWACS ROUGE", MenuSupport, ActivateOverlord )
 MENU_MISSION_COMMAND:New( "TANKER BLEU", MenuSupport, ActivateTexaco )
 MENU_MISSION_COMMAND:New( "TANKER ROUGE", MenuSupport, ActivateArco )
 
---MENU_MISSION_COMMAND:New( "POULET BLEU", MenuTest, ActivateGroup, "poulet1" )
---MENU_MISSION_COMMAND:New( "POULET ROUGE", MenuTest, ActivateGroup, "poulet2" )
---MENU_MISSION_COMMAND:New( "PORC ROUGE", MenuTest, ActivateGroup, "porc1" )
+MENU_MISSION_COMMAND:New( "POULET BLEU", MenuTest, ActivateGroup, "poulet1" )
+MENU_MISSION_COMMAND:New( "POULET ROUGE", MenuTest, ActivateGroup, "poulet2" )
+MENU_MISSION_COMMAND:New( "PORC ROUGE", MenuTest, ActivateGroup, "porc1" )
 
+
+PatrolZones = {}
+BlueZoneGroup = GROUP:FindByName( "Blue Zone" )
+BlueZone = ZONE_POLYGON:New( "Blue Zone", BlueZoneGroup )
+RedZoneGroup = GROUP:FindByName( "Red Zone" )
+RedZone = ZONE_POLYGON:New( "Red Zone", RedZoneGroup )
+YellowZoneGroup = GROUP:FindByName( "Yellow Zone" )
+YellowZone = ZONE_POLYGON:New( "Yellow Zone", YellowZoneGroup )
 
 
